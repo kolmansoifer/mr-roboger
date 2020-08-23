899cfc91ecfceb49dcb405272387902e2a0969ac
kolmansoifer <kolmansoifer@gmail.com>
2020-08-23 12:08:03 -0700

    Complete basic js

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,24 @@
 
   // User Interface
-$(document).ready(function(){
-  $("form#robot").submit(function(event){
+$(document).ready(function() {
+  $("#robot").submit(function(event) {
     event.preventDefault();
     $(".result").hide();
+    const input = parseInt($("input#number").val());
 
-    const input = parseInt
+    if (input === 1) {
+      $(".result").text("Bleep!");
+    }
+    else if (input === 2) {
+      $(".result").text("Bloop!");
+    }
+    else if (input === 3) {
+      $(".result").text("Won't you be my neighbor?")
+    }
 
-
-
+    $(".result").text();
+    $(".result").show();
+        
 
   })
 });    
-
-
-
-
-
-
-// Business Interface
-/*
-function friendlyNumber(number) {
-  let numberArray = []
-  for (let i = 0; i <= number; i=+) {
-    numberArray.push(i.toString());
-  }
-  numberArray.forEach(function(number)) {
-    if (number.indexOf("1") >= 0) {
-      numberArray{}
-  
-  let arrayInput = userInput.split()
-  if (userInput <=0) {
-    alert("Please enter a human number greater than 0"); 
-  } 
-    
-  $(".input").text(userInput);
-
-};
-/*
-
-// Behavior Order:
-
-// Describe: `mr-robogers-neighborhood()`
-// Test: The program will return a range of numbers from 0 to the users inputted number.
-// Expect(friendlyNumber(5)).toEqual("1,2,3,4,5");
-
-// Test: The program will convert an input that contains 1 into "Beep!"
-// Expect(friendlyNumber(1)).toEqual("Beep!");
-
-// Test: The program will convert an input that contains 2 into "Boop!"
-// Expect(friendlyNumber(2)).toEqual("Boop!")'
-
-// Test: The program will convert an input that contains 3 into "Won't you be my neighbor?"
-// Expect(friendlyNumber(3)).toEqual("Won't you be my neighbor?");
